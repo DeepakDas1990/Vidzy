f52543afbdaadaa4ac6445fa2d3ec9bd67fda334
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-06-08 21:22:27 +0530

    Add project files.

---
+++ Vidzy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DbContext = new VidzyDbContext();
+            DbContext.AddVideo("Zurassic World", new DateTime(2016, 9, 23), 1);
+            DbContext.AddVideo("The Lord of The Rings", new DateTime(2001,6, 12), 4);
+            DbContext.AddVideo("The Hateful Eight", new DateTime(2007, 8, 9), 4);
+            DbContext.SaveChanges();
+        }
+    }
+}
